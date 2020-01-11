884ca1cdea6d123cee7615ede4583147ef6267a4
kirikzyusko1996 <zyusko.kirik@gmail.com>
2020-01-11 18:33:55 +0300

    [DL-06] - #4 task completed (added results saving)

--- 1/main.py
+++ 1/main.py
@@ -4,7 +4,7 @@ import matplotlib.image as mpimg
 from sklearn.model_selection import train_test_split
 from goto import with_goto
 import imageio
-import hashlib
+import numpy as np
 
 letters = ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J')
 TRAIN_DATA_PATH = os.path.join('..', 'data', 'notMNIST_large')
@@ -13,12 +13,34 @@ TEST_DATA_PATH = os.path.join('..', 'data', 'notMNIST_small')
 if not os.path.isdir(TRAIN_DATA_PATH) or not os.path.isdir(TEST_DATA_PATH):
     raise Exception(f'No data found in path: {TRAIN_DATA_PATH}')
 
+def to_tuple(x):
+    return tuple(tuple(tuple(l2) for l2 in l1) for l1 in x)
 
-def get_overlaps(images1, images2):
-    hash1 = set([hashlib.md5(str(image1).encode('utf-8')) for image1 in images1])
-    hash2 = set([hashlib.md5(str(image2).encode('utf-8')) for image2 in images2])
-    all_overlaps = set.intersection(hash1, hash2)
-    return all_overlaps
+def get_overlaps(X1, X2, y1, y2):
+    x_1 = to_tuple(X1)
+    x_2 = to_tuple(X2)
+    dict1 = {}
+    dict2 = {}
+    set1 = set()
+    set2 = set()
+    for i in range(len(x_1)):
+        dict1[x_1[i]] = y1[i]
+    for i in range(len(x_2)):
+        dict2[x_2[i]] = y2[i]
+    del x_1
+    del x_2
+    set1.update(dict1)
+    set2.update(dict2)
+    diff = set.difference(set1, set2)
+    del set1
+    del set2
+    del dict2
+    x = []
+    y = []
+    for i in diff:
+        x.append(i)
+        y.append(dict1[i])
+    return x, y
 
 
 def get_dataset(dataset_path):
@@ -94,19 +116,39 @@ def main():
 
     # 3
     label .task
+    x1 = [[[1, 2], [3, 4]], [[5, 6], [7, 8]]]
+    y1 = [1,7]
+    x2 = [[[1, 2], [3, 4]], [[6, 7], [7, 8]]]
+    y2 = [1,1]
+    X, y = get_overlaps(x1, x2, y1, y2)
+    print(np.load('X.npy'))
+    print(np.load('y.npy'))
+    np.save('X', np.array(X))
+    np.save('y', np.array(y))
     X, y = transform_to_array(cut_dataset(get_dataset(TRAIN_DATA_PATH), 0, 21000))
     X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.047619, random_state=1)
     X_test, y_test = transform_to_array(get_dataset(TEST_DATA_PATH))
 
     # 4
     print('Files info fetched...')
-    X_train = load_images(X_train, y_train, TRAIN_DATA_PATH)
-    X_val = load_images(X_val, y_val, TRAIN_DATA_PATH)
-    X_test = load_images(X_test, y_test, TEST_DATA_PATH)
-    print(get_overlaps(X_train, X_val))
-    print(get_overlaps(X_train, X_test))
+    X_train, y_train = load_images(X_train, y_train, TRAIN_DATA_PATH)
+    X_val, y_val = load_images(X_val, y_val, TRAIN_DATA_PATH)
+    X_test, y_test = load_images(X_test, y_test, TEST_DATA_PATH)
+    print('Images loaded into memory...')
+    print(len(X_train)) # 200.000
+    X_without_val, y_without_val = get_overlaps(X_train, X_val, y_train, y_val)
+    print(len(X_without_val)) # 174.808
+    X_without_val_test, y_without_val_test = get_overlaps(X_without_val, X_test, y_without_val, y_test)
+    print(len(X_without_val_test)) # 171.395
+    np.save('X_train', np.array(X_without_val_test))
+    np.save('y_train', np.array(y_without_val_test))
+    np.save('X_val', np.array(X_val))
+    np.save('y_val', np.array(y_val))
+    np.save('X_text', np.array(X_test))
+    np.save('y_test', np.array(y_test))
 
     # 5
+
 
 
 if __name__ == '__main__':
