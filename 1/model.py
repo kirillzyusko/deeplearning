dc159d42a78ec599025d16ec2a1918a2337daad8
kirikzyusko1996 <zyusko.kirik@gmail.com>
2020-01-13 11:28:25 +0300

    [DL-09] - loaders added

--- 1/model.py
+++ 1/model.py
@@ -1,24 +1,12 @@
 from sklearn.linear_model import LogisticRegression
-import numpy as np
-import collections
 
 import warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
 
+import sys, os
+sys.path.append(os.path.abspath(os.path.join('..', 'loaders')))
 
-def load():
-    X_train = convert_3d_to_2d(np.load('../processed/notMNIST/X_train.npy')) / 255 # with normalization
-    y_train = np.load('../processed/notMNIST/y_train.npy')
-    X_test = convert_3d_to_2d(np.load('../processed/notMNIST/X_test.npy')) / 255 # with normalization
-    y_test = np.load('../processed/notMNIST/y_test.npy')
-    # {'G': 17495, 'E': 17446, 'B': 17445, 'A': 17441, 'F': 17382, 'C': 17315, 'D': 17315, 'J': 17256, 'H': 17104, 'I': 15196}
-    print(collections.Counter(y_train))
-
-    return X_train, y_train, X_test, y_test
-
-
-def convert_3d_to_2d(arr):
-    return np.reshape(arr, [arr.shape[0], arr.shape[1] * arr.shape[2]])
+from load_not_mnist import load
 
 
 X_train, y_train, X_test, y_test = load()
---
+++ loaders/load_not_mnist.py
@@ -0,0 +1,16 @@
+import numpy as np
+import collections
+
+def load():
+    X_train = convert_3d_to_2d(np.load('../processed/notMNIST/X_train.npy')) / 255 # with normalization
+    y_train = np.load('../processed/notMNIST/y_train.npy')
+    X_test = convert_3d_to_2d(np.load('../processed/notMNIST/X_test.npy')) / 255 # with normalization
+    y_test = np.load('../processed/notMNIST/y_test.npy')
+    # {'G': 17495, 'E': 17446, 'B': 17445, 'A': 17441, 'F': 17382, 'C': 17315, 'D': 17315, 'J': 17256, 'H': 17104, 'I': 15196}
+    print(collections.Counter(y_train))
+
+    return X_train, y_train, X_test, y_test
+
+
+def convert_3d_to_2d(arr):
+    return np.reshape(arr, [arr.shape[0], arr.shape[1] * arr.shape[2]])
