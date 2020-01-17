e667c2241650e2d49eb47fd69fc1793fa37baf93
kirikzyusko1996 <zyusko.kirik@gmail.com>
2020-01-17 11:56:07 +0300

    [DL-11] - #2 lab finished

--- 2/main.py
+++ 2/main.py
@@ -2,8 +2,10 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras import regularizers
 from keras.layers import Dropout
+from keras import optimizers
 import pandas as pd
 import numpy as np
+import tensorflow as tf
 
 import warnings
 warnings.filterwarnings("ignore", category=FutureWarning)
@@ -16,24 +18,31 @@ from load_not_mnist import load
 
 X_train, y_train, X_test, y_test = load()
 
-# definition
-model = Sequential()
+with tf.device('/cpu:0'):
+    # definition
+    model = Sequential()
 
-# adding layers
-model.add(Dense(200, kernel_initializer='normal', activation='relu', name='hidden_0'))
-model.add(Dense(140, kernel_initializer='normal', activation='relu', name='hidden_1'))
-model.add(Dense(100, kernel_initializer='normal', activation='relu', name='hidden_2'))
-model.add(Dense(70, kernel_initializer='normal', activation='relu', name='hidden_3'))
-model.add(Dense(10, kernel_initializer='normal', activation='relu', name='hidden_4'))
+    # adding layers
+    model.add(Dense(200, kernel_initializer='normal', activation='relu', kernel_regularizer=regularizers.l2(0.001), name='hidden_0'))
+    model.add(Dropout(0.1, name='dropout_0'))
+    model.add(Dense(170, kernel_initializer='normal', activation='relu', kernel_regularizer=regularizers.l2(0.001), name='hidden_1'))
+    model.add(Dropout(0.1, name='dropout_1'))
+    model.add(Dense(130, kernel_initializer='normal', activation='relu', kernel_regularizer=regularizers.l2(0.001), name='hidden_2'))
+    model.add(Dropout(0.1, name='dropout_2'))
+    model.add(Dense(100, kernel_initializer='normal', activation='relu', kernel_regularizer=regularizers.l2(0.001), name='hidden_3'))
+    model.add(Dropout(0.1, name='dropout_3'))
+    model.add(Dense(10, kernel_initializer='normal', activation='relu', name='hidden_4'))
 
-# compile model
-model.compile(loss='mse', optimizer='sgd', metrics=['accuracy'])
+    sgd = optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
 
-# train
-model.fit(X_train, np.array(pd.get_dummies(y_train)),
-          epochs=50,
-          verbose=1,
-          validation_data=(X_test, np.array(pd.get_dummies(y_test))))
+    # compile model
+    model.compile(loss='mse', optimizer=sgd, metrics=['accuracy'])
 
-score, acc = model.evaluate(X_test, np.array(pd.get_dummies(y_test)), verbose=1)
-print(score, acc) # 0.92, 0.0210 - 37 epochs
+    # train
+    model.fit(X_train, np.array(pd.get_dummies(y_train)),
+              epochs=1000,
+              verbose=1,
+              validation_data=(X_test, np.array(pd.get_dummies(y_test))))
+
+    score, acc = model.evaluate(X_test, np.array(pd.get_dummies(y_test)), verbose=1)
+    print(score, acc) # 0.92, 0.0328 - 60 epochs
