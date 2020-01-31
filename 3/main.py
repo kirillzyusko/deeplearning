dbe2a21d65b4e2a17f22778a7bfa14696fe74856
kiryl.zyusko <kiryl.ziusko@itechart-group.com>
2020-01-31 14:38:35 +0300

    [DL-03] - added LeNet5

--- 3/main.py
+++ 3/main.py
@@ -1,6 +1,41 @@
-import os
+from keras.models import Sequential
+from keras.layers import Dense, Conv2D, MaxPool2D, Flatten, Dropout
+import pandas as pd
+import numpy as np
+import tensorflow as tf
 
-DATA_PATH = os.path.join('../data', "notMNIST_large")
+import warnings
+warnings.filterwarnings("ignore", category=FutureWarning)
 
-if not os.path.isdir(DATA_PATH):
-    raise Exception(f'No data found in path: {DATA_PATH}')
+import sys, os
+sys.path.append(os.path.abspath(os.path.join('..', 'loaders')))
+
+from load_not_mnist import load
+
+
+X_train, y_train, X_test, y_test = load(conv=True)
+
+with tf.device('/cpu:0'):
+    # definition
+    model = Sequential()
+
+    # adding layers
+    model.add(Conv2D(16, 5, input_shape=(28, 28, 1), padding='same', activation='relu'))
+    model.add(MaxPool2D())
+    model.add(Conv2D(32, 5, padding='same', activation='relu'))
+    model.add(MaxPool2D())
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dropout(0.25))
+    model.add(Dense(10, activation='softmax'))
+
+    model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['acc'])
+
+    # train
+    model.fit(X_train, np.array(pd.get_dummies(y_train)),
+              epochs=1000,
+              verbose=1,
+              validation_data=(X_test, np.array(pd.get_dummies(y_test))))
+
+    score, acc = model.evaluate(X_test, np.array(pd.get_dummies(y_test)), verbose=1)
+    print(score, acc)  # 0.92, 0.0328 - 60 epochs
--- loaders/load_not_mnist.py
+++ loaders/load_not_mnist.py
@@ -1,10 +1,11 @@
 import numpy as np
 import collections
 
-def load():
-    X_train = convert_3d_to_2d(np.load('../processed/notMNIST/X_train.npy')) / 255 # with normalization
+
+def load(conv=False):
+    X_train = convert_3d_to_2d(np.load('../processed/notMNIST/X_train.npy'), conv) / 255  # with normalization
     y_train = np.load('../processed/notMNIST/y_train.npy')
-    X_test = convert_3d_to_2d(np.load('../processed/notMNIST/X_test.npy')) / 255 # with normalization
+    X_test = convert_3d_to_2d(np.load('../processed/notMNIST/X_test.npy'), conv) / 255  # with normalization
     y_test = np.load('../processed/notMNIST/y_test.npy')
     # {'G': 17495, 'E': 17446, 'B': 17445, 'A': 17441, 'F': 17382, 'C': 17315, 'D': 17315, 'J': 17256, 'H': 17104, 'I': 15196}
     print(collections.Counter(y_train))
@@ -12,5 +13,5 @@ def load():
     return X_train, y_train, X_test, y_test
 
 
-def convert_3d_to_2d(arr):
-    return np.reshape(arr, [arr.shape[0], arr.shape[1] * arr.shape[2]])
+def convert_3d_to_2d(arr, conv):
+    return np.reshape(arr, [arr.shape[0], arr.shape[1] * arr.shape[2]]) if conv else np.reshape(arr, [arr.shape[0], arr.shape[1], arr.shape[2], 1])
